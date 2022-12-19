c53692ca674f043accc919f1c28ab953ac155a44
gzbrdr <gabrielzbardini@gmail.com>
2022-12-19 01:03:24 -0300

    asdasd

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const dataDiv = document.getElementById('useData')
 const mainDiv = document.getElementById('main')
-
-const gameMonsters = ['Noggin', 'Mammott', 'Toe', 'Potbelly', 'Fwog', 'Shrubb', 'Furcorn']
-
+const allMonsters = []
+const gameMonsters = ['Noggin', 'Mammott', 'Toe Jammer', 'Potbelly', 'Fwog', 'Shrubb', 'Furcorn', 'Oaktopus', 'Maw', 'Drumpler', 'Pummel', 'Bowgart', 'Clamble', 'T-Rox', 'Entbrat', 'Ghazt', 'Punkleton','Do', 'Re', 'Mi', 'Fa', 'Sol', 'La', 'Ti', 'Shugabush', 'Tawkerr', 'Parlsona', `G'joob`, 'Wubbox']
+const cards = {}
 async function main() {
     await getPage()
     getContent()
@@ -17,7 +17,7 @@ async function getPage() {
             dataDiv.innerHTML = data
         })
   } catch (error) {
-    console.error(error);
+    mainDiv.innerHTML = 'you need CORS<br><a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf">https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf</a>'
   }
 }
 
@@ -41,35 +41,69 @@ function getRealLink(link) {
 
 function getMonsterName(link) {
     const monsterName = link.split('/')[7];
-    if (monsterName.includes('Element') ||
-        monsterName.includes('%'))
+    if (monsterName.includes('Element'))
         return
     return monsterName.replace('.png', '').replace('_', ' ')
+}
+
+function filterMonsters(monster) {
+    for (const gameMonster of gameMonsters) {
+        monster = decodeURI(monster)
+        if (monster.split(' ').pop() === decodeURI(gameMonster))
+            return monster
+    }
+    return undefined
+}
+
+function filterLinks(links) {
+    let futureMainDiv = ''
+    links.forEach(link => {
+        if (!link)
+            return
+        let monsterName = getMonsterName(decodeURI(link))
+        monsterName = filterMonsters(monsterName)
+        if (!monsterName)
+            return
+        const realLink = getRealLink(link)
+        console.log(monsterName, realLink);
+        futureMainDiv += `<div class="monsterWrapper"><img class="monster" src="${realLink}"/>${monsterName}</div>`
+        const monsterObj = {}
+        monsterObj[monsterName] = realLink
+        allMonsters.push(monsterObj)
+    })
+    mainDiv.innerHTML = futureMainDiv
+    mainDiv.style.display = 'flex'
+    createMonstersObject()
 }
 
 function getContent() {
     const content = document.getElementById('content')
     const tables = content.getElementsByClassName('article-table-custom-border')
     let monsters
-    let futureMainDiv = ''
     Array.from(tables).forEach(table => {
         mainDiv.innerHTML += table.innerHTML
         monsters = mainDiv.getElementsByTagName('a')
     })
     dataDiv.innerHTML = ''
+    let links = []
     Array.from(monsters).forEach(monster => {
         const monsterImg = monster.getElementsByTagName('img')
-        const link = $(monsterImg).attr('data-src')
-        if (!link)
-            return
-        const monsterName = getMonsterName(link)
-        if (!monsterName)
-            return
-        const realLink = getRealLink(link)
-        console.log(monsterName, realLink);
-        futureMainDiv += `<div class="monsterWrapper"><img class="monster" src="${realLink}"/>${monsterName}</div>`
+        links.push($(monsterImg).attr('data-src'))
     })
-    mainDiv.innerHTML = futureMainDiv
-    mainDiv.style.display = 'flex'
-    mainDiv.textContent
+    filterLinks(links)
+}
+
+function createMonstersObject() {
+    gameMonsters.forEach(gameMonster => {
+        const monstersTypes = []
+        allMonsters.forEach(monster => {
+            monster = Object.keys(monster)[0]
+            console.log(monster);
+            if(monster.includes(gameMonster)) {
+                monstersTypes.push(monster)
+            }
+        })
+        cards[gameMonster] = monstersTypes
+    })
+    console.log(cards);
 }
